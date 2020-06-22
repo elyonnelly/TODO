8f6a2161f731f8a9f18e122f811e3002b2eb0da6
Alena Fedorova <Alena.Fedorova@kaspersky.com>
2020-06-22 13:17:33 +0300

    notifyWork added

---
+++ app/src/main/java/com/example/todo/notification/NotifyWork.kt
@@ -0,0 +1,7 @@
+package com.example.todo.notification
+
+import android.content.Context
+import io.reactivex.Scheduler
+
+class NotifyWork(context: Context, params: WorkerParameters) : Scheduler.Worker(context, params) {
+}
